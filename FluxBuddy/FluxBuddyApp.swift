553be60897aa7de57fe8c9678a49edde78916dc6
Virosso <virosso4499@gmail.com>
2025-12-22 17:48:36 +0100

    Initial Commit

---
+++ FluxBuddy/FluxBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxBuddyApp.swift
+//  FluxBuddy
+//
+//  Created by Matúš Juhász on 22/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FluxBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
